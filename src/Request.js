a3d47c81c2719936ca942847a3a776b79cf8914f
Rushabh Dabhade <47267236+Coder-Rushabh@users.noreply.github.com>
2023-10-24 14:51:20 +0530

    Add files via upload

---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = '35a78d7f48bd23621a7c3da95ad69481'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
